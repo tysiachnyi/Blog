e7324f3823b23cdb3bd0abe22e81167b794585c5
Nikita Tysiachnyi <nikita.tysiachnyi@gmail.com>
2020-04-11 22:13:59 +0300

    added interface

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {User} from "../../shared/interfaces";
 
 @Component({
   selector: 'app-login-page',
@@ -29,6 +30,11 @@ export class LoginPageComponent implements OnInit {
     console.log(this.form);
     if (this.form.invalid) {
       return
+    }
+
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
     }
   }
 
