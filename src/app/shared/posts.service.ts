5b7fb6733c7cae003cac77f739ab747b952e55bb
Nikita Tysiachnyi <nikita.tysiachnyi@gmail.com>
2020-04-15 22:47:52 +0300

    added get allPosts from firebase and using routing for post

--- src/app/shared/posts.service.ts
+++ src/app/shared/posts.service.ts
@@ -24,4 +24,17 @@ export class PostService {
         );
   }
 
+  getAll(): Observable<Post[]> {
+      return this.http.get(`${environment.fbDBUrl}/posts.json`)
+          .pipe(map((response: {[key: string]: any}) => {
+             return Object
+                 .keys(response)
+                 .map(key => ({
+                     ...response[key],
+                     id: key,
+                     date: new Date(response[key].date)
+                  }))
+          }))
+  }
+
 }
