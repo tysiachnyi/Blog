765ea88764d5c69941be926f8d8f251ce47f0799
Nikita Tisyachniy <nikita.tisyachniy@lifecell.com.ua>
2020-04-15 15:47:51 +0300

    Added error handler

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {User} from "../../shared/interfaces";
-import {AuthService} from "../shared/services/auth.service";
-import {Router} from "@angular/router";
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {User} from '../../shared/interfaces';
+import {AuthService} from '../shared/services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -14,7 +14,7 @@ export class LoginPageComponent implements OnInit {
   form: FormGroup;
   submitted = false;
 
-  constructor(private auth: AuthService,
+  constructor(public auth: AuthService,
               private router: Router) { }
 
   ngOnInit() {
@@ -27,12 +27,12 @@ export class LoginPageComponent implements OnInit {
           Validators.required,
           Validators.minLength(6)
       ]),
-    })
+    });
   }
 
   submit() {
     if (this.form.invalid) {
-      return
+      return;
     }
 
     this.submitted = true;
@@ -40,12 +40,15 @@ export class LoginPageComponent implements OnInit {
     const user: User = {
       email: this.form.value.email,
       password: this.form.value.password
-    }
+    };
+
     this.auth.login(user).subscribe(() => {
       this.form.reset();
-      this.router.navigate(['/admin','dashboard']);
+      this.router.navigate(['/admin', 'dashboard']);
       this.submitted = false;
-    })
+    }, () => {
+      this.submitted = false;
+    });
   }
 
 }
